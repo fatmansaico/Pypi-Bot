bfd66f2f5917af41f49faa755e30799b0254b490
Fat Man Saico <91099042+fatmansaico@users.noreply.github.com>
2021-09-20 18:07:38 -0700

    Delete config.py

--- pypibot/core/config.py
+++
@@ -1,6 +0,0 @@
-class Config():
-    def __init__(self) -> None:
-        self.token = ""
-        self.owner_id = 875208952516861953
-        self.data_path = "./pypibot/data/database.db"
-        self.build_path = "./pypibot/data/script.sql"
